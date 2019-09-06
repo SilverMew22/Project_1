1a80571c8e1d8b50c5f51318176f6d8793ece870
Shequana <ShequanaGarnett@gmail.com>
2019-09-06 01:54:12 -0400

    Added Unity

---
+++ Unity_for_Class/Scripts and Components/Assets/CapsuleMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(10 * Mathf.Sin(Time.time), 10 * Mathf.Cos(Time.time), 0);
+
+        // General waveform equation.
+        // output = amplitude * sin(frequency * T + frequencyOffset) + baselineOffset;
+    }
+}
